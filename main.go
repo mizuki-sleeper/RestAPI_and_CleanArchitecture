37b5a752a68f5f8a60c6c491bd8b15104f90b6ae
mizuki-sleeper <roko.k@hotmail.co.jp>
2023-07-13 19:14:58 +0900

    First commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"echo-rest-api/controller"
+	"echo-rest-api/db"
+	"echo-rest-api/repository"
+	"echo-rest-api/router"
+	"echo-rest-api/usecase"
+	"echo-rest-api/validator"
+)
+
+func main() {
+	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	taskValidator := validator.NewTaskValidator()
+	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	userController := controller.NewUserController(userUsecase)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ migrate/migrate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"echo-rest-api/db"
+	model "echo-rest-api/model"
+	"fmt"
+)
+
+func main() {
+	dbConn := db.NewDB()
+	defer fmt.Println("Successfully Migrated")
+	defer db.CloseDB(dbConn)
+	dbConn.AutoMigrate(&model.User{}, &model.Task{})
+}
